f00523d6c420c8c1cc416528f5cb24bc2d7685b7
Lopez_Anthonee <lopea468@hsd.k12.or.us>
2025-05-30 09:55:59 -0700

    finished 3.1

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    private float speed = 30;
+    private PlayerController playerControllerScript;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<playerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerControllerScript.gameOver == false) {
+        transform.Translate(Vector3.left * Time.deltaTime * speed); }   
+    }
+}
